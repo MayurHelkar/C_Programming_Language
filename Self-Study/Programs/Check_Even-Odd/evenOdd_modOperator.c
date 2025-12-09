94d1a24db1bebaadf843f238938814eee6ba5e9a
Mayur Helkar <mayurhelkar@yahoo.com>
2025-12-09 16:06:35 +0530

    Add files via upload

---
+++ Self-Study/Programs/Check_Even-Odd/evenOdd_modOperator.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+
+int evenOdd_ifElse(int);
+int evenOdd_switchCase(int);
+
+int main()
+{
+    int evenOddVar;
+    int res;
+
+    printf("Enter a Number to check if it is Even or Odd : ");
+    scanf("%d", &evenOddVar);
+//    printf("checking the %d value for Even or Odd\n", evenOddVar);
+
+    (evenOddVar % 2)? printf("Number is Odd\n") : printf("Number is Even\n");
+
+//    res = evenOdd_ifElse(evenOddVar);
+    res = evenOdd_switchCase(evenOddVar);
+    if(res == 1)
+    {
+        printf("Result for Even-Odd : Odd");
+    }
+    else
+    {
+        printf("Result of Even-Odd : Even");
+    }
+    
+    printf("\n");
+
+    return 0;
+}
+
+int evenOdd_ifElse(int num)
+{
+    if(num % 2 == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+
+    return -1;
+}
+
+int evenOdd_switchCase(int Num)
+{
+    switch(Num % 2)
+    {
+        case 1:
+        {
+            return 1;
+//            break;
+        }
+        case 0:
+        {
+            return 0;
+//            break;
+        }
+        default :
+        {
+            return -1;
+            break;
+        }
+    }
+    return -1;
+}
