16055f5f680ba9e060b97e261d3123891400b8a2
Mayur Helkar <mayurhelkar@yahoo.com>
2025-12-18 10:40:49 +0530

    Add files via upload

---
+++ Programs/Swap_Values/swapValues_singleLine_arithmeticAddSubtract.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;          int b;
+    
+    printf("Input 2 values ::\n");
+    scanf("%d", &a);                    scanf("%d", &b);
+
+    printf("\n");
+
+    printf("Before Swapping ::\n");
+    printf("a : %d\t\t", a);          printf("b : %d\n", b);
+    
+//    b = (a + b) - (a = b);
+    a += b - (b = a);
+    
+    printf("\n");
+
+    printf("After Swapping ::\n");
+    printf("a : %d\t\t", a);          printf("b : %d\n", b);
+    
+    return 0;
+}
