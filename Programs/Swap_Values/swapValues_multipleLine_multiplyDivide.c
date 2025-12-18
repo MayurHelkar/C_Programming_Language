537d9da680f2c25dd3cea409e827705e22705ef5
Mayur Helkar <mayurhelkar@yahoo.com>
2025-12-18 10:56:43 +0530

    Add files via upload

---
+++ Programs/Swap_Values/swapValues_multipleLine_multiplyDivide.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;          int b;
+    
+    printf("Input 2 values ::\n");
+    scanf("%d", &a);                    scanf("%d", &b);
+
+    printf("\n");
+
+    printf("Before Swapping ::\n");
+    printf("a : %d\t\t", a);          printf("b : %d\n", b);
+    
+    a = a * b;
+    b = a / b;
+    a = a / b;
+    
+    printf("\n");
+
+    printf("After Swapping ::\n");
+    printf("a : %d\t\t", a);          printf("b : %d\n", b);
+    
+    return 0;
+}
