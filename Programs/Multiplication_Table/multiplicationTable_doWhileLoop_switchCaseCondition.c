5977b1e77c61f3921495469fa85b265bb294c980
Mayur Helkar <mayurhelkar@yahoo.com>
2025-12-19 17:54:47 +0530

    Add files via upload

---
+++ Programs/Multiplication_Table/multiplicationTable_doWhileLoop_switchCaseCondition.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+int main()
+{
+    int Number;
+    int loop = 1;
+
+    printf("Number to print Multiplication Table : ");
+    scanf("%d", &Number);
+
+    printf("%d Multiplication table ::\n", Number);
+    do
+    {
+        switch(Number)
+        {
+            case 1:
+            {
+                printf("%d * %d = %d\n", Number, loop, loop);
+                break;
+            }
+            case 2:
+            {
+                printf("%d * %d = %d\n", Number, loop, loop << 1);
+                break;
+            }
+            case 3:
+            {
+                printf("%d * %d = %d\n", Number, loop, (loop << 1) + loop);
+                break;
+            }
+            case 4:
+            {
+                printf("%d * %d = %d\n", Number, loop, (loop << 2));
+                break;
+            }
+            case 5:
+            {
+                printf("%d * %d = %d\n", Number, loop, (loop << 2) + loop);
+                break;
+            }
+            case 6:
+            {
+                printf("%d * %d = %d\n", Number, loop, (loop << 2) + (loop << 1));
+                break;
+            }
+            case 7:
+            {
+                printf("%d * %d = %d\n", Number, loop, (loop << 2) + (loop << 1) + loop);
+                break;
+            }
+            case 8:
+            {
+                printf("%d * %d = %d\n", Number, loop, loop << 3);
+                break;
+            }
+            case 9:
+            {
+                printf("%d * %d = %d\n", Number, loop, (loop << 3) + loop);
+                break;
+            }
+            case 10:
+            {
+                printf("%d * %d = %d\n", Number, loop, (loop << 3) + (loop << 1));
+                break;
+            }
+        }
+        loop++;
+    }
+    while(loop < 11);
+
+    return 0;
+}
